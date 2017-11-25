95751e6856acdf062b1f2f441a36c685d15dee64
Catie <cecowden@utexas.edu>
2017-11-24 22:04:16 -0600

    Modifying variable names

--- ASLRestima/heap_entropy.c
+++ ASLRestima/heap_entropy.c
@@ -8,18 +8,19 @@ int main()
     //void *brk = NULL;
 
     /* sbrk(0) gives current program break location */
-    void * address= NULL;
-    address = sbrk(0);
+    void * addr= NULL;
+    addr = sbrk(0);
     //printf("%p\n", address);
+    
     char binary[65] = {0};
     //unsigned int mask;
-    long address2 = (long)address;
-    //printf("%lu\n", address2);
+    long address = (long)addr;
+    //printf("%lu\n", address);
 
     int i;
     long k;
     for (i = 63; i >= 0; i--) {
-        k = address2 >> i;
+        k = address >> i;
         if (k & 1)
             strcat(binary, "1");
         else
