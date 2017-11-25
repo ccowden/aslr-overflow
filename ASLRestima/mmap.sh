1492cfc84ed7154a6511d81a2dc8ee6f47853c5f
Catie <cecowden@utexas.edu>
2017-11-24 22:05:14 -0600

    Initial check in for mmap

---
+++ ASLRestima/mmap.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# CATCHING AND IGNORING SIGNAL 15 HELPS AVOID HANGS
+trap "" 15
+
+ulimit -c 0
+
+for run in `jot 800 1`
+do
+  printf \\r$run
+  timeout -s 9 60 ./mmap_entropy >> $1
+  sleep 2
+done
