5e7a394281c26abd9a91cb49c5970b703f783a43
Catie <cecowden@utexas.edu>
2017-12-02 13:39:07 -0600

    Added echo statements to show steps

--- ASLRestima/getBitEntropy.sh
+++ ASLRestima/getBitEntropy.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
+echo "Usage: ./getBitEntropy.sh rawLogFile outputFile"
+
 #make sure the Ent code is built
+echo "Building ENT"
 cd Ent
-make
+make > /dev/null
 cd ..
 
 #put randomized bits (excludes first 16, last 12 bits) into a temp file
 #also remove new lines by writing to a temp file
+echo "Reformatting input file"
 while read line
 do
     printf ${line:16:36}
 done < $1 > temp.txt
 
-#run the Ent code
+echo "Executing ent code"
 "Ent/ent" temp.txt -c > $2
+
+echo "Results saved in $2"
 
 #clean up
 rm temp.txt
