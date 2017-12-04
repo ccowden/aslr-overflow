af39c4b667d7dddd2b1798608e8338e5cfb6e7bd
Catie <cecowden@utexas.edu>
2017-12-04 11:58:47 -0600

    Clean up script. Call Distribution script.

--- ASLRestima/plotEverything.sh
+++ ASLRestima/plotEverything.sh
@@ -17,19 +17,30 @@ make -f Makefile
 #Iterate through all segments of memory. Create logs and decimal files if needed. Generate plots.
 for i in "${types[@]}"
 do
-    if [ ! -f "$i"Log.txt ]; then
-        echo "$i""Log.txt not found. Running $i.sh to create it."
-        ./$i.sh "$i""Log.txt"
-    fi
-    if [ ! -f "$i"Decimal.txt ]; then
-        echo "$i""Decimal.txt not found. Running binaryToDec64.sh to create it."
-        ./binaryToDec64.sh "$i""Log.txt" > "$i""Decimal.txt"
-    fi
-
     YAXIS="${capitalTypes[$i]}"" Starting Address"
     TITLE="Memory Address Entropy for ""${capitalTypes[$i]}"" - 64 bit ""${OPSYSTEM}"
     FLATTITLE="Flattened ""$TITLE"
+    DISTRIBTITLE="Distribution of ""$TITLE"
+    LOGFILE="$i""Log.txt"
+    BASHSCRIPT="$i.sh"
     DECIMALFILE="$i""Decimal.txt"
+    DECFULLFILE="$i""DecFull.txt"
+
+    if [ ! -f "$LOGFILE" ]; then
+        echo "$LOGFILE not found. Running $BASHSCRIPT to create it."
+        ./$BASHSCRIPT $LOGFILE
+    fi
+    if [ ! -f "$DECIMALFILE" ]; then
+        echo "$DECIMALFILE not found. Running binaryToDec64.sh to create it."
+        ./binaryToDec64.sh "$LOGFILE" > "$DECIMALFILE"
+    fi
+
+    if [ ! -f "$DECFULLFILE" ]; then
+        echo "$DECFULLFILE not found. Running binaryToDecFull.sh to create it."
+        ./binaryToDecFull.sh $LOGFILE > $DECFULLFILE
+    fi
+
     python plot64.addresses.py "$DECIMALFILE" "$YAXIS" "$TITLE"
     python flatten64.addresses.py "$DECIMALFILE" "$YAXIS" "$FLATTITLE"
+    python distrib64.addresses.py "$DECFULLFILE" "COUNT" "$DISTRIBTITLE"
 done
