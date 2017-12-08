6c4e061544a70b4c9dea570c5239f147573cfff3
Vaseekaran Rajagopal <vaseekaran@utexas.edu>
2017-12-08 18:56:43 -0500

    mmap large page entropy estimator

---
+++ ASLRestima/mmaplarge.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# CATCHING AND IGNORING SIGNAL 15 HELPS AVOID HANGS
+trap "" 15
+
+ulimit -c 0
+
+> $1
+
+for run in `jot 800 1`
+do
+  printf \\r$run
+  timeout -s 9 60 ./mmaplarge_entropy >> $1
+done
