d62ed116af104cb4433b0a465a4bde4f0fb497a7
Catie <cecowden@utexas.edu>
2017-12-08 20:49:58 -0600

    Adding ASLRestimaLog file

--- ASLRestima/ASLRestima.sh
+++ ASLRestima/ASLRestima.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
+echo ""
+echo "Welcome to ASLRestima. Beginning computations..."
+
 ######################## Source the config file ########################
 . ./ASLRestima.config
+LOG=$LOGFILENAME
+REPORT=$REPORTFILENAME
+echo "Initiating ASLRestima" > $LOG
+
+######################## Clean up, if requested ########################
+if [ "$CLEANUP" = true ]; then
+    echo "Beginning clean up!" >> $LOG
+    make superclean >> $LOG
+    echo "" >> $LOG
+fi
 
 ######################## Build memory segments ########################
-echo "Building all memory segments..."
-make all -f Makefile
+make all -f Makefile >> $LOG
 
-echo "Building ENT..."
+echo "Building ENT framework" >> $LOG
 cd Ent
-make > /dev/null
+make >> $LOG
 cd ..
-
-echo "*************************************"
+echo "Successfully built required files" >> $LOG
+echo "" >> $LOG
 
 ######################## Function to Call a Memory Segment Finder #####################
 function loop_mem_segment() {
@@ -20,70 +32,94 @@ function loop_mem_segment() {
     
     ## $1 is the memory segment the function is applied to.
     ## Empty file before starting to write to it.
-    > "${logFiles[$1]}"
+    > "${binaryFiles[$1]}"
 
     # Gather $ITERATIONS samples of starting addresses
     for run in `jot $ITERATIONS 1`
     do
         printf \\r$run > /dev/null
-        timeout -s 9 60 "./${findAddress[$1]}" >> "${logFiles[$1]}"
+        timeout -s 9 60 "./${findAddress[$1]}" >> "${binaryFiles[$1]}"
     done
 }
 
 ######################## Loop through Memory Segments ########################
 for i in "${types[@]}"
 do
+    echo "Examining the $i"
+
     ########## Define Variables ##########
     YAXIS="${capitalTypes[$i]}"" Starting Address"
     TITLE="Memory Address Entropy for ""${capitalTypes[$i]}"" - 64 bit ""${OPSYSTEM}"
     FLATTITLE="Flattened ""$TITLE"
     DISTRIBTITLE="Distribution of ""$TITLE"
-    LOGFILE=${logFiles[$i]}
+    BINARYFILE=${binaryFiles[$i]}
     DECIMALFILE="$i""Decimal.txt"
     DECFULLFILE="$i""DecFull.txt"
     BITLENGTH=${lengths[$i]}
     FIRSTBIT=${startPos[$i]}
     ENTFILE="$i""Entropy.txt"
 
-    ########## Build Log/Decimal/Full files ##########
-    if [ ! -f "$LOGFILE" ]; then
-        echo "$i: $LOGFILE not found. Creating it for you."
+    ########## Build Binary/Decimal/Full files ##########
+    if [ ! -f "$BINARYFILE" ]; then
+        echo "$i: Creating $BINARYFILE" >> $LOG
         loop_mem_segment $i
     fi
 
     if [ ! -f "$DECIMALFILE" ]; then
-        echo "$i: $DECIMALFILE not found. Running binaryToDec64.sh to create it."
-        ./binaryToDec64.sh "$LOGFILE" > "$DECIMALFILE"
+        echo "$i: Running binaryToDec64.sh to create $DECIMALFILE" >> $LOG
+        ./binaryToDec64.sh "$BINARYFILE" > "$DECIMALFILE"
     fi
 
     if [ ! -f "$DECFULLFILE" ]; then
-        echo "$i: $DECFULLFILE not found. Running binaryToDecFull.sh to create it."
-        ./binaryToDecFull.sh $LOGFILE > $DECFULLFILE
+        echo "$i: Running binaryToDecFull.sh to create $DECFULLFILE" >> $LOG
+        ./binaryToDecFull.sh $BINARYFILE > $DECFULLFILE
     fi
+
+    echo "$i: Successfully gathered $ITERATIONS starting addresses." >> $LOG
 
     ########## Execute Python scripts for plotting ##########
     if [ ! -d "graphs" ]; then
+        echo "" >> $LOG
+        echo "Creating graphs directoring for plots" >> $LOG
+        echo "" >> $LOG
         mkdir "graphs"
     fi
 
+    echo "$i: Creating plot of all $ITERATIONS addresses" >> $LOG
     python plot64.addresses.py "$DECIMALFILE" "$SUPPRESSPLOTS" "$YAXIS" "$TITLE"
+
+    echo "$i: Creating flattened plot of all $ITERATIONS addresses" >> $LOG
     python flatten64.addresses.py "$DECIMALFILE" "$SUPPRESSPLOTS" "$YAXIS" "$FLATTITLE"
+
+    echo "$i: Creating histogram to illustrate distribution of addresses" >> $LOG
     python distrib64.addresses.py "$DECFULLFILE" "$SUPPRESSPLOTS" "$NUMBEROFBINS" "COUNT" "$DISTRIBTITLE"
-    echo "$i: Finished creating plots."
+
+    echo "$i: Successfully created all 3 plots" >> $LOG
 
     ########## Run the ENT Statistics ##########
-    echo "$i: Finished evaluating entropy for $BITLENGTH bits starting at bit $FIRSTBIT."
+    echo "$i: Finished evaluating entropy for $BITLENGTH bits starting at bit $FIRSTBIT." >> $LOG
     while read line 
     do
         printf ${line:$FIRSTBIT:$BITLENGTH}
-    done < $LOGFILE > temp.txt
+    done < $BINARYFILE > temp.txt
 
     "Ent/ent" temp.txt -ct > $ENTFILE
 
-    echo "$i: Finished measuring entropy statistics."
+    echo "$i: Finished measuring entropy statistics." >> $LOG
 
     ########## Clean up ##########
     rm temp.txt
 
-    echo "$i: Finished running script."
+    echo "$i: Successfully completed all operations" >> $LOG
+    echo "" >> $LOG
 done
+
+######################## Clean up, if requested ########################
+if [ "$CLEANUP" = true ]; then
+    echo "Beginning clean up!" >> $LOG
+    make superclean >> $LOG
+    echo "" >> $LOG
+fi
+
+
+echo "All done! Please check the graphs directory for your plots, $REPORT for a summary of the results, and $LOG for debugging information."
