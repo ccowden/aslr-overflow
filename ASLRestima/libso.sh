5e791719bb9be497b226065bf0f426ecc7702051
Catie <cecowden@utexas.edu>
2017-12-08 19:29:14 -0600

    Removing .sh files, logic is now in aslrestima.sh

--- ASLRestima/libso.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-> $1
-
-for run in `jot 800 1`
-do
-  printf \\r$run
-  ./libso_entropy >> $1
-done
--- ASLRestima/text.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-> $1
-
-for run in `jot 800 1`
-do
-  printf \\r$run
-  ./text_entropy >> $1
-done
